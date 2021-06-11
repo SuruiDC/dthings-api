c426cf53b6d38c9b0ede3fdd01685565f6a48ecc
SuruiDC <73009341+SuruiDC@users.noreply.github.com>
2021-06-11 18:57:22 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const { getInfoBot } = require("./functions/getInfoBot")
 const { getUser } = require("./functions/getUser")
 
-getUser("618634689204322314").then(console.log)
 module.exports = {
 	getInfoBot,
 	getUser
